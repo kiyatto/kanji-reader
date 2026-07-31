99c37de53b68022665d38880dc61a5606636d2ed
Johann Ong <katreeya.ong@hotmail.com>
2026-07-30 20:49:24 -0700

    cuda pytorch + png to npy change in extraction flow

--- extract.py
+++ extract.py
@@ -1,4 +1,4 @@
-# converts ETL9G images (after processing) and labels into Numpy arrays
+# converts ETL9G images (after processing from binary format to png) and labels into Numpy arrays
 
 import os
 import numpy as np
@@ -11,8 +11,12 @@ class_list = sorted([f for f in os.listdir(data_path)
                       if os.path.isdir(os.path.join(data_path, f))])
 class_dict = { name: i for i, name in enumerate(class_list) }
 
+print("Found {} classes: {}".format(len(class_list), class_list))
+
 images = []
 labels = []
+
+print("Extracting images and labels from folders...")
 
 for folder_name in class_list:
     folder_path = os.path.join(data_path, folder_name)
@@ -27,5 +31,9 @@ for folder_name in class_list:
 X = np.stack(images);
 Y = np.array(labels);
 
-np.savez_compressed('kanji_data.npz', images=X, labels=Y, class_list = class_list)
+print("Now saving the data to kanji_images.npy, kanji_labels.npy, and class_list.npy...")
+
+np.save("kanji_images.npy", X)
+np.save("kanji_labels.npy", Y)
+np.save("class_list.npy", np.array(class_list))
 
